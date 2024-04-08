e920c6d161d3fd6ce348b72f575e78fa8d7ee66b
Starwhooper <thiemo@schuff.eu>
2024-04-08 19:44:51 +0200

    Update comparedisplay.py

--- comparedisplay.py
+++ comparedisplay.py
@@ -25,6 +25,7 @@ try:
  from PIL import Image
  from PIL import ImageDraw
  from urllib.parse import quote
+ import RPi.GPIO as GPIO
  import json
  import logging
  import os
@@ -53,6 +54,11 @@ try:
 except:
  logging.critical('file ' + cf['luma']['demo_opts.py']['folder'] + '/demo_opts.py not found. Please check config.json or do sudo git clone https://github.com/rm-hull/luma.examples /opt/luma.examples')
  sys.exit("\033[91m {}\033[00m" .format('file ' + cf['luma']['demo_opts.py']['folder'] + '/demo_opts.py not found. Please check config.json or do sudo git clone https://github.com/rm-hull/luma.examples /opt/luma.examples'))
+
+KEY_PRESS_PIN  = 13
+GPIO.setmode(GPIO.BCM) 
+GPIO.cleanup()
+GPIO.setup(KEY_PRESS_PIN,   GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
 
 def inverterurl():
  return('http://' + cf['inverter']['user'] + ':' + quote(cf['inverter']['pw']) + '@' + cf['inverter']['address'] + '/' + cf['inverter']['site'])
@@ -135,9 +141,6 @@ def calculate():
  except: inverter_time = datetime(1977, 1, 1)
  
  global inverter_total
- try: inverter_total
- except: inverter_total = 0
- 
  global inverter_adj
  
  if (inverter_time <= datetime.now() - timedelta(minutes=1)) or inverter_now == 0:
@@ -147,26 +150,17 @@ def calculate():
    inverter_total = inv_t
    inverter_time = datetime.now()
   else:
-   inverter_total = inverter_total
-   logging.warning('inverter total count not found')
+   try: inverter_total
+   except: 
+    inverter_total = 0
+    logging.warning('inverter total count not found')
   
   inverter_adj = inverter_total + cf['inverter']['offset']  
 
-#calculate electricitymeter
- global electricitymeter_now
- try: electricitymeter_now
- except: electricitymeter_now = 0
- 
- global electricitymeter_total_in
- try: electricitymeter_total_in
- except: electricitymeter_total_in = 0
- 
- global electricitymeter_total_out
- try: electricitymeter_total_out
- except: electricitymeter_total_out = 0
- 
  e_total_in, e_total_out, e_now = readelectricitymeter()
- 
+
+ global electricitymeter_total_in
+ global electricitymeter_total_out
  if e_total_in >= 1:
   electricitymeter_total_in = e_total_in
   electricitymeter_total_out = e_total_out
@@ -179,11 +173,37 @@ def calculate():
   electricitymeter_total_out = electricitymeter_total_out
   logging.info('all electricitymeter information set to 0')
  
+ global powersource
+ if e_now <= 0: powersource = 'sun'
+ else: powersource = 'net'
+
+ global electricitymeter_now
  electricitymeter_now = e_now
 
 #calculate others 
  global consumption
  consumption = electricitymeter_now + inverter_now
+ 
+ #########compare current consumption and current provided over inverter to know how much of current consumption cames from sun
+ global rateconsumptionfromsun
+ try:
+  rateconsumptionfromsun = int(imagewidth / consumption * inverter_now)
+ except:
+  rateconsumptionfromsun = 0
+ 
+ #########compare current provided over inverter and current consumption to know how much of current solar power are used from my household
+ global ratesolarpowerforhousehold
+ try:
+  ratesolarpowerforhousehold = int(imagewidth / inverter_now * consumption)
+ except:
+  ratesolarpowerforhousehold = 0
+
+ #########compare complete provided from interver exclude the adjustemt with the complete provided over electricitymeter out to net to know how much of collected sun energy i use myself
+ global rateinverteradjvselectricimetertotalout
+ try:
+  rateinverteradjvselectricimetertotalout = int(imagewidth - (imagewidth / inverter_adj * electricitymeter_total_out))
+ except:
+  rateinverteradjvselectricimetertotalout = 0
 
 #######################################################
 #
@@ -191,6 +211,13 @@ def calculate():
 #
 #######################################################
 
+def colorbar(rate):
+ if rate > 80: color = 'green'
+ elif rate > 60 < imagewidth*0.8: color = 'orange'
+ elif rate > 40 < imagewidth*0.8: color = 'yellow'
+ else: color = 'red'
+ return(color)
+ 
 def createimage(imagewidth,imageheight):
  global outputimage
  global sunbeam
@@ -199,91 +226,125 @@ def createimage(imagewidth,imageheight):
  global sunkwh
  try: sunkwh
  except: sunkwh = 'tot'
+ global imagestyle
+ try: imagestyle
+ except: imagestyle = cf['imagestyle']
  
  outputimage = Image.new(mode="RGB", size=(imagewidth,imageheight))
- 
  draw = ImageDraw.Draw(outputimage)
 
- #######house
- draw.rectangle([(40,50),(imagewidth-40,imagewidth-20-15)], fill = "black", outline = "white", width = 3)
- draw.line([(40-3,50+3),(imagewidth/2,26)], fill = "red", width = 6) #left roof
- draw.line([(imagewidth-40+3,50+3),(imagewidth/2,26)], fill = "red", width = 6) #right roof
- draw.text((45,65), str(consumption) + 'W', font = font, fill = 'Yellow')
- draw.text((45,75), str(electricitymeter_total_in) + 'kWh', font = font, fill = 'Yellow')
+ if GPIO.input(KEY_PRESS_PIN) == 0: # button is released
+  if imagestyle == 'detail':
+   imagestyle = 'pretty'
+   logging.info('changes imagestyle to pretty')
+  elif imagestyle == 'pretty':
+   imagestyle = 'detail'
+   logging.info('changes imagestyle to detail')
+  
+ if imagestyle == 'detail':
+  detailfont = ImageFont.truetype(cf['font']['ttffile'], 10)#cf['font']['ttfsize'])
+  draw.text((0,0),  'consumtion: ' + str(consumption) + 'W', font = detailfont, fill = 'white')
+  draw.text((0,10), 'net total in: ' + str(electricitymeter_total_in) + 'kWh', font = detailfont, fill = 'white')
+  draw.text((0,20), 'sun total in: ????', font = detailfont, fill = 'white')
+  draw.text((0,30), 'inverter adj : ' + str(round(inverter_adj)) + 'kWh', font = detailfont, fill = 'white')
+  draw.text((0,40), 'inverter total: ' + str(round(inverter_total)) + 'kWh', font = detailfont, fill = 'white')
+  draw.text((0,50), 'inverter now :' + str(inverter_now) + 'W', font = detailfont, fill = 'white')
+  draw.text((0,60), 'net now :' + str(electricitymeter_now) + 'W', font = detailfont, fill = 'white')
+  draw.text((0,70), 'net total out :' + str(electricitymeter_total_out) + 'kWh', font = detailfont, fill = 'white')
+  draw.text((0,80), 'powered by ' + powersource, font = detailfont, fill = 'white')
+  draw.text((0,90), 'cur. PV energy cons.', font = detailfont, fill = 'white')  #########compare current consumption and current provided over inverter to know how much of current consumption cames from sun
+  try: draw.rectangle([(0,90,int(imagewidth / 100 * rateconsumptionfromsun),99)], fill = colorbar(rateconsumptionfromsun), width = 1)
+  except: pass
+  draw.text((0,100), 'cur. use of PV energy', font = detailfont, fill = 'white')  #########compare current provided over inverter and current consumption to know how much of current solar power are used from my household
+  try: draw.rectangle((0,100,int(imagewidth / 100 * ratesolarpowerforhousehold),109), fill = colorbar(ratesolarpowerforhousehold), width = 1)
+  except: pass
+  draw.text((0,110), str(round(inverter_adj)) + ' vs. ' + str(round(electricitymeter_total_out)), font = detailfont, fill = 'white') #########compare complete provided from interver exclude the adjustemt with the complete provided over electricitymeter out to net to know how much of collected sun energy i use myself
+  draw.rectangle((0,110,int(imagewidth / 100 * rateinverteradjvselectricimetertotalout),119), fill = colorbar(rateinverteradjvselectricimetertotalout), width = 1)
 
- #######sun
- draw.ellipse([(-40,-40),(40,40)], fill = "yellow")
- if inverter_now >= 1:
-  pass
-  draw.ellipse([(-40-sunbeam,-40-sunbeam),(40+sunbeam,40+sunbeam)], outline = "yellow")
-  sunbeam=sunbeam+4
-  if sunbeam >= 4*4: sunbeam = 0
- draw.text((1,1), 'total:', font = font, fill = 'black')
- if inverter_total == 0: draw.text((1,11), '????', font = font, fill = 'black')
- else: 
-  if sunkwh == 'tot':
-   draw.text((1,11), str(round(inverter_adj)), font = font, fill = 'black')
-   sunkwh = 'adj'
+ if imagestyle == 'pretty':
+  #######house
+  #facade
+  draw.rectangle([(40,50),(imagewidth-40,imagewidth-20-15)], fill = "black", outline = "white", width = 3)
+  #roof
+  draw.line([(40-3,50+3),(imagewidth/2,26)], fill = "red", width = 6) #left roof
+  draw.line([(imagewidth-40+3,50+3),(imagewidth/2,26)], fill = "red", width = 6) #right roof
+  #text
+  draw.text((45,65), str(consumption) + 'W', font = font, fill = 'Yellow')
+  draw.text((45,75), str(electricitymeter_total_in) + 'kWh', font = font, fill = 'Yellow')
+ 
+  #######sun
+  draw.ellipse([(-40,-40),(40,40)], fill = "yellow")
+  if inverter_now >= 1:
+   pass
+   draw.ellipse([(-40-sunbeam,-40-sunbeam),(40+sunbeam,40+sunbeam)], outline = "yellow")
+   sunbeam=sunbeam+4
+   if sunbeam >= 4*4: sunbeam = 0
+  draw.text((1,1), 'total:', font = font, fill = 'black')
+  if inverter_total == 0: draw.text((1,11), '????', font = font, fill = 'black')
   else: 
-   draw.text((1,11), '(' + str(round(inverter_total)) + ')', font = font, fill = 'black')
-   sunkwh = 'tot'
-  
- draw.text((1,21), 'kWh', font = font, fill = 'black')
- left, top, right, bottom = draw.textbbox((10,50), str(inverter_now) + 'W', font=font)
- draw.rectangle((left-1, top-1, right+1, bottom+1), fill="black")
- draw.text((10,50), str(inverter_now) + 'W', font = font, fill = 'white')
-
- #######powerline
- draw.line([(imagewidth-15,10),(imagewidth-20,40)], fill = "gray", width = 2)
- draw.line([(imagewidth-15,10),(imagewidth-10,40)], fill = "gray", width = 2)
- draw.line([(imagewidth-25,20),(imagewidth-5,20)], fill = "gray", width = 2)
- draw.line([(imagewidth-23,25),(imagewidth-7,25)], fill = "gray", width = 2)
- draw.text((imagewidth-30,50), str(electricitymeter_now) + 'W', font = font, fill = 'white')
- draw.text((imagewidth-60,5), str(electricitymeter_total_out) + 'kWh', font = font, fill = 'white')
+   if sunkwh == 'tot':
+    draw.text((1,11), str(round(inverter_adj)), font = font, fill = 'black')
+    sunkwh = 'adj'
+   else: 
+    draw.text((1,11), '(' + str(round(inverter_total)) + ')', font = font, fill = 'black')
+    sunkwh = 'tot'
+   
+  draw.text((1,21), 'kWh', font = font, fill = 'black')
+  left, top, right, bottom = draw.textbbox((10,50), str(inverter_now) + 'W', font=font)
+  draw.rectangle((left-1, top-1, right+1, bottom+1), fill="black")
+  draw.text((10,50), str(inverter_now) + 'W', font = font, fill = 'white')
  
- #######note
- if (electricitymeter_now > 0): draw.text((25,95), 'powered by net', font = font, fill = 'Yellow')
- if (electricitymeter_now < 0): draw.text((25,95), 'powered by sun', font = font, fill = 'Yellow')
-
- 
- if (int(inverter_now) > 0):
- #########compare current consumption and current provided over inverter to know how much of current consumption cames from sun
-  try:
-   rate = int(imagewidth / consumption * inverter_now)
-  except:
-   rate = 0
-   logging.warning('division zero: consumption = ' + str(consumption))
-
-  if rate < imagewidth*0.6: color = 'red'
-  elif rate < imagewidth*0.8: color = 'orange'
-  else: color = 'green'
-  draw.line([(0,107),(rate,107)], fill = color, width = 4)
-  draw.text((0,112), 'cur. PV energy cons.', font = font, fill = 'Yellow')
-
- #########compare current provided over inverter and current consumption to know how much of current solar power are used from my household
-  try:
-   rate = int(imagewidth / inverter_now * consumption)
-  except:
-   rate = 0
-   logging.warning('division zero: inverter_now = ' + str(inverter_now))
-  if rate < imagewidth*0.6: color = 'red'
-  elif rate < imagewidth*0.8: color = 'orange'
-  else: color = 'green'
+  #######powerline
+  draw.line([(imagewidth-15,10),(imagewidth-20,40)], fill = "gray", width = 2)
+  draw.line([(imagewidth-15,10),(imagewidth-10,40)], fill = "gray", width = 2)
+  draw.line([(imagewidth-25,20),(imagewidth-5,20)], fill = "gray", width = 2)
+  draw.line([(imagewidth-23,25),(imagewidth-7,25)], fill = "gray", width = 2)
+  draw.text((imagewidth-30,50), str(electricitymeter_now) + 'W', font = font, fill = 'white')
+  draw.text((imagewidth-60,5), str(electricitymeter_total_out) + 'kWh', font = font, fill = 'white')
   
-  draw.rectangle((0,121,rate,124), fill = color)
-
- #########compare complete provided from interver exclude the adjustemt with the complete provided over electricitymeter out to net to know how much of collected sun energy i use myself
-  try:
-   rate = int(imagewidth - (imagewidth / inverter_adj * electricitymeter_total_out))
-  except:
-   rate = 0
-   logging.warning('division zero: inverter_adj = ' + str(inverter_adj))
-  if rate < imagewidth*0.3: color = 'red'
-  elif rate < imagewidth*0.6: color = 'orange'
-  else: color = 'green'
-  draw.rectangle((0,125,rate,127), fill = color)
-  draw.text((0,imageheight-10), str(round(inverter_adj)), font = font, fill = 'white')
-  draw.text((imagewidth-10,imageheight-10), str(round(electricitymeter_total_out)), font = font, fill = 'white')
+  #######note
+  if (electricitymeter_now > 0): draw.text((25,95), 'powered by net', font = font, fill = 'Yellow')
+  if (electricitymeter_now < 0): draw.text((25,95), 'powered by sun', font = font, fill = 'Yellow')
+ 
+  
+  if (int(inverter_now) > 0):
+  #########compare current consumption and current provided over inverter to know how much of current consumption cames from sun
+   try:
+    rate = int(imagewidth / consumption * inverter_now)
+   except:
+    rate = 0
+    logging.warning('division zero: consumption = ' + str(consumption))
+ 
+   if rate < imagewidth*0.6: color = 'red'
+   elif rate < imagewidth*0.8: color = 'orange'
+   else: color = 'green'
+   draw.line([(0,107),(rate,107)], fill = color, width = 4)
+   draw.text((0,112), 'cur. PV energy cons.', font = font, fill = 'Yellow')
+ 
+  #########compare current provided over inverter and current consumption to know how much of current solar power are used from my household
+   try:
+    rate = int(imagewidth / inverter_now * consumption)
+   except:
+    rate = 0
+    logging.warning('division zero: inverter_now = ' + str(inverter_now))
+   if rate < imagewidth*0.6: color = 'red'
+   elif rate < imagewidth*0.8: color = 'orange'
+   else: color = 'green'
+   
+   draw.rectangle((0,121,rate,124), fill = color)
+ 
+  #########compare complete provided from interver exclude the adjustemt with the complete provided over electricitymeter out to net to know how much of collected sun energy i use myself
+   try:
+    rate = int(imagewidth - (imagewidth / inverter_adj * electricitymeter_total_out))
+   except:
+    rate = 0
+    logging.warning('division zero: inverter_adj = ' + str(inverter_adj))
+   if rate < imagewidth*0.3: color = 'red'
+   elif rate < imagewidth*0.6: color = 'orange'
+   else: color = 'green'
+   draw.rectangle((0,125,rate,127), fill = color)
+   draw.text((0,imageheight-10), str(round(inverter_adj)), font = font, fill = 'white')
+   draw.text((imagewidth-10,imageheight-10), str(round(electricitymeter_total_out)), font = font, fill = 'white')
 
 #######################################################
 #
@@ -292,7 +353,14 @@ def createimage(imagewidth,imageheight):
 #######################################################
 
 def output(device):
- #outputimage.save(os.environ['HOME'] + '/poweroutput.gif')
+ global lastimageexport
+ try: lastimageexport
+ except: lastimageexport = datetime(1977, 1, 1)
+ if lastimageexport <= datetime.now() - timedelta(seconds=cf['imageexport']['intervall']):
+  if cf['imageexport']['active'] == True:
+   outputimage.save(eval(cf['imageexport']['path']))
+   lastimageexport = datetime.now()
+   logging.info('saved current displaycontent to: ' + eval(cf['imageexport']['path']))
  device.display(outputimage)
 
 #######################################################
